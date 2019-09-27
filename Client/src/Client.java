88af964e19727b6da62d5a5be583cfc0d22b052e
Rohan Shahane <shahane.rohan96@gmail.com>
2019-09-27 15:55:24 -0400

    Initial commit

---
+++ Client/src/Client.java
@@ -0,0 +1,75 @@
+import java.net.*;
+import java.io.*;
+import java.nio.*;
+import java.nio.channels.*;
+import java.util.*;
+
+public class Client {
+    Socket requestSocket;           //socket connect to the server
+    ObjectOutputStream out;         //stream write to the socket
+    ObjectInputStream in;          //stream read from the socket
+    String message;                //message send to the server
+    String MESSAGE;                //capitalized message read from the server
+
+    public static void main(String args[]) {
+        Client client = new Client();
+        client.run();
+    }
+
+    //public void Client() {}
+
+    void run() {
+        try{
+            //create a socket to connect to the server
+            requestSocket = new Socket("localhost", 8000);
+            System.out.println("Connected to localhost in port 8000");
+            //initialize inputStream and outputStream
+            out = new ObjectOutputStream(requestSocket.getOutputStream());
+            out.flush();
+            in = new ObjectInputStream(requestSocket.getInputStream());
+
+            //get Input from standard input
+            BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+            while(true)	{
+                System.out.print("Hello, please input a sentence: ");
+                message = bufferedReader.readLine();				//read a sentence from the standard input
+                sendMessage(message); 								//Send the sentence to the server
+                MESSAGE = (String)in.readObject();					//Receive the upperCase sentence from the server
+                System.out.println("Receive message: " + MESSAGE);	//show the message to the user
+            }
+        }
+        catch (ConnectException e) {
+            System.err.println("Connection refused. You need to initiate a server first.");
+        }
+        catch ( ClassNotFoundException e ) {
+            System.err.println("Class not found");
+        }
+        catch(UnknownHostException unknownHost){
+            System.err.println("You are trying to connect to an unknown host!");
+        }
+        catch(IOException ioException){
+            ioException.printStackTrace();
+        }
+        finally{													//Close connections
+            try{
+                in.close();
+                out.close();
+                requestSocket.close();
+            }
+            catch(IOException ioException){
+                ioException.printStackTrace();
+            }
+        }
+    }
+
+    void sendMessage(String msg) {									//send a message to the output stream
+        try{														//stream write the message
+            out.writeObject(msg);
+            out.flush();
+            System.out.println("Send message: " + msg);
+        }
+        catch(IOException ioException){
+            ioException.printStackTrace();
+        }
+    }
+}
---
+++ Server/src/Server.java
@@ -0,0 +1,69 @@
+import java.net.*;
+import java.io.*;
+import java.nio.*;
+import java.nio.channels.*;
+import java.util.*;
+
+public class Server {
+    int sPort = 8000;    //The server will be listening on this port number
+    ServerSocket sSocket;   //serversocket used to listen on port number 8000
+    Socket connection = null; //socket for the connection with the client
+    String message;    //message received from the client
+    String MESSAGE;    //uppercase message send to the client
+    ObjectOutputStream out;  //stream write to the socket
+    ObjectInputStream in;    //stream read from the socket
+
+    public static void main(String args[]) {
+        Server s = new Server();
+        s.run();
+    }
+    //public void Server() {}
+    void run() {
+        try {
+
+            sSocket = new ServerSocket(sPort, 10);										//create a serversocket
+            System.out.println("Waiting for connection");								//Wait for connection
+            connection = sSocket.accept();												//accept a connection from the client
+            System.out.println("Connection received from " + connection.getInetAddress().getHostName());
+            out = new ObjectOutputStream(connection.getOutputStream());					//initialize Input and Output streams
+            out.flush();
+            in = new ObjectInputStream(connection.getInputStream());
+
+            try {
+                while(true) {
+                    message = (String)in.readObject();							//receive the message sent from the client
+                    System.out.println("Receive message: " + message);			//show the message to the user
+                    MESSAGE = message.toUpperCase();							//Capitalize all letters in the message
+                    sendMessage(MESSAGE);										//send MESSAGE back to the client
+                }
+            }
+            catch(ClassNotFoundException classnot){
+                System.err.println("Data received in unknown format");
+            }
+        }
+        catch(IOException ioException){
+            ioException.printStackTrace();
+        }
+        finally{
+            try{									//Close connections
+                in.close();
+                out.close();
+                sSocket.close();
+            }
+            catch(IOException ioException){
+                ioException.printStackTrace();
+            }
+        }
+    }
+
+    void sendMessage(String msg) {										//send a message to the output stream
+        try{
+            out.writeObject(msg);
+            out.flush();
+            System.out.println("Send message: " + msg);
+        }
+        catch(IOException ioException){
+            ioException.printStackTrace();
+        }
+    }
+}
