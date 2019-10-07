a0e6c6c814af5f8031bf650b638e5426c705894c
Rohan Shahane <shahane.rohan96@gmail.com>
2019-10-07 13:48:07 -0400

    Refractor
    Refractored the code to improve redability and reduce confusion.

--- Server/src/Server.java
+++ Server/src/Server.java
@@ -1,44 +1,38 @@
-
-
-
 import java.net.*;
 import java.io.*;
-import java.nio.*;
-import java.nio.channels.*;
-import java.security.MessageDigest;
-import java.util.*;
+
 
 public class Server {
     int sPort = 8000;    //The server will be listening on this port number
-    ServerSocket sSocket;   //serversocket used to listen on port number 8000
-    Socket connection = null; //socket for the connection with the client
+    ServerSocket serverSocket;   //serversocket used to listen on port number 8000
+    Socket localSocket = null; //socket for the connection with the client
     String message;    //message received from the client
     String MESSAGE;    //uppercase message send to the client
     String ListOfDir;
-    ObjectOutputStream out;  //stream write to the socket
-    ObjectInputStream in;    //stream read from the socket
-    FileInputStream fis = null;
-    BufferedInputStream bis = null;
-    OutputStream os = null;
+    ObjectOutputStream objectOutputStream;  //stream write to the socket
+    ObjectInputStream objectInputStream;    //stream read from the socket
+    FileInputStream fileInputStream = null;
+    BufferedInputStream bufferedInputStream = null;
+    OutputStream outputStream = null;
     public static void main(String args[]) {
-        Server s = new Server();
-        s.run();
+        Server server = new Server();
+        server.run();
     }
     //public void Server() {}
     void run() {
         try {
             final String pathOfFileServer = "F:\\UF Acad\\Sem 1\\Computer Networks\\Project\\FTPServer\\";
-            sSocket = new ServerSocket(sPort, 10);										//create a serversocket
+            serverSocket = new ServerSocket(sPort, 10);										//create a serversocket
             System.out.println("Waiting for connection");								//Wait for connection
-            connection = sSocket.accept();												//accept a connection from the client
-            System.out.println("Connection received from " + connection.getInetAddress().getHostName());
-            out = new ObjectOutputStream(connection.getOutputStream());					//initialize Input and Output streams
-            out.flush();
-            in = new ObjectInputStream(connection.getInputStream());
+            localSocket = serverSocket.accept();												//accept a connection from the client
+            System.out.println("Connection received from " + localSocket.getInetAddress().getHostName());
+            objectOutputStream = new ObjectOutputStream(localSocket.getOutputStream());					//initialize Input and Output streams
+            objectOutputStream.flush();
+            objectInputStream = new ObjectInputStream(localSocket.getInputStream());
 
             try {
                 while(true) {
-                    message = (String)in.readObject();							//receive the message sent from the client
+                    message = (String) objectInputStream.readObject();							//receive the message sent from the client
                     System.out.println("Receive message: " + message);			//show the message to the user
                     String [] inputParamArr = message.split("\\s");
                     if(message.toString().toLowerCase().equals("dir")){
@@ -56,18 +50,18 @@ public class Server {
                             // send file
                             File myFile = new File (filePath);
                             byte [] mybytearray  = new byte [(int)myFile.length()];
-                            fis = new FileInputStream(myFile);
-                            bis = new BufferedInputStream(fis);
-                            bis.read(mybytearray,0,mybytearray.length);
-                            os = connection.getOutputStream();
+                            fileInputStream = new FileInputStream(myFile);
+                            bufferedInputStream = new BufferedInputStream(fileInputStream);
+                            bufferedInputStream.read(mybytearray,0,mybytearray.length);
+                            outputStream = localSocket.getOutputStream();
                             System.out.println("Sending " + filePath + "(" + mybytearray.length + " bytes)");
-                            os.write(mybytearray,0,mybytearray.length);
-                            os.flush();
+                            outputStream.write(mybytearray,0,mybytearray.length);
+                            outputStream.flush();
                             System.out.println("Done.");
                         }
                         finally {
-                            if (bis != null) bis.close();
-                            if (os != null) os.close();
+                            if (bufferedInputStream != null) bufferedInputStream.close();
+                            if (outputStream != null) outputStream.close();
                             //if (sock!=null) sock.close();
                         }
                     }
@@ -86,9 +80,9 @@ public class Server {
         }
         finally{
             try{									//Close connections
-                in.close();
-                out.close();
-                sSocket.close();
+                objectInputStream.close();
+                objectOutputStream.close();
+                serverSocket.close();
             }
             catch(IOException ioException){
                 ioException.printStackTrace();
@@ -98,8 +92,8 @@ public class Server {
 
     void sendMessage(String msg) {										//send a message to the output stream
         try{
-            out.writeObject(msg);
-            out.flush();
+            objectOutputStream.writeObject(msg);
+            objectOutputStream.flush();
             System.out.println("Send message: " + msg);
         }
         catch(IOException ioException){
